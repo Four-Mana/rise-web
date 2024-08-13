1f200090e3f95e3d22b1e313f2fec4ad1fc5317a
TigreDev <donowenrt@gmail.com>
2024-08-13 20:29:42 +0200

    initial commit

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,16 @@
+
+export const Header = () => {
+    return (
+        <nav className="w-full px-6 py-2 fixed top-0">
+            <div className="flex justify-between items-center">
+                <div>
+                    left container
+                </div>
+                <div>
+                    right container
+                </div>
+            </div>
+        </nav>
+    )
+}
+
---
+++ src/sanity/schemaTypes/homePage.ts
@@ -0,0 +1,75 @@
+import { defineField, defineType } from "sanity";
+
+export default defineType({
+    name: 'home',
+    title: 'Home page',
+    type: 'document',
+    groups: [
+        { name: 'seo', title: 'SEO' },
+        { name: 'hero', title: 'Hero' }
+    ],
+    fields: [
+        defineField({
+            name: 'slug',
+            title: 'Slug',
+            type: 'slug',
+            group: 'seo',
+            validation: (Rule) => [Rule.required()],
+        }),
+        defineField({
+            name: 'title',
+            title: 'Title',
+            type: 'string',
+            group: 'seo',
+            validation: (Rule) => [Rule.required()],
+        }),
+        defineField({
+            name: 'description',
+            title: 'description',
+            type: 'text',
+            rows: 3,
+            group: 'seo',
+            validation: (Rule) => [Rule.required()],
+        }),
+        defineField({
+            type: 'object',
+            name: 'backgroundImage',
+            group: 'hero',
+            fields: [
+                defineField({
+                    name: 'image',
+                    title: 'Image',
+                    type: 'image',
+                    validation: (Rule) => [Rule.required()],
+                    fields: [{
+                        name: 'alt',
+                        type: 'string',
+                        title: 'Image description',
+                        hidden: ({ parent }) => !parent?.asset,
+                        validation: (Rule) => [Rule.required()],
+                    }]
+                })
+            ]
+        }),
+        defineField({
+            type: 'object',
+            name: 'frontImage',
+            group: 'hero',
+            fields: [
+                defineField({
+                    name: 'image',
+                    title: 'Image',
+                    type: 'image',
+                    validation: (Rule) => [Rule.required()],
+                    fields: [{
+                        name: 'alt',
+                        type: 'string',
+                        title: 'Image description',
+                        hidden: ({ parent }) => !parent?.asset,
+                        validation: (Rule) => [Rule.required()],
+                    }]
+                })
+            ]
+        }),
+    ]
+})
