c30054e9a7e6eb7af8d67d6dc71b2af1678c374f
TigreDev <donowenrt@gmail.com>
2024-08-15 11:32:43 +0200

    add utils

--- src/components/navbar/DesktopNavbar.tsx
+++ src/components/navbar/DesktopNavbar.tsx
@@ -1,13 +1,23 @@
+'use client'
+import { useHasPastHeight } from "@/hooks";
+import Link from "next/link"
 
 export const DesktopNavbar = () => {
+    const isPastNavbar = useHasPastHeight(80);
+
     return (
-        <nav className="w-full px-6 py-2 fixed top-0">
+        <nav className={`w-full px-6 py-2 fixed top-0 transition-all duration-200 ease-out ${isPastNavbar ? 'bg-black shadow-lg' : ' bg-transparent'}`}>
+
             <div className="flex justify-between items-center">
                 <div>
-                    left container
+                    <Link href='/'>
+                        🐯
+                    </Link>
                 </div>
-                <div>
-                    right container
+                <div className="flex gap-2">
+                    <Link href='/discografia'>
+                        Discografia
+                    </Link>
                 </div>
             </div>
         </nav>
---
+++ src/request/utils.ts
@@ -0,0 +1,13 @@
+
+export const sanityConfig = {
+    projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+    dataset: 'prod',
+    apiVersion: '2024-08-13',
+    useCdn: false
+}
+
+export const getSanityUrl = (query: string) => {
+    const { projectId, dataset, apiVersion, useCdn } = sanityConfig;
+    const encodedQuery = encodeURI(query).replace(/%20/g, '+');
+    return `https://${projectId}.${useCdn ? 'apicdn' : 'api'}.sanity.io/v${apiVersion}/data/query/${dataset}?query=${encodedQuery}`;
+}
