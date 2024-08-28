a2fc93b49bbde1b33818afff5087749acb5eb832
TigreDev <donowenrt@gmail.com>
2024-08-28 23:11:18 +0200

    some previous work

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { HeroSection } from "@/components/heroSection";
 
 export default function Home() {
   return (
-    <main className="flex h-[300vh] flex-col items-center justify-between ">
+    <main className="flex h-[300vh] flex-col items-center justify-between">
       <HeroSection />
     </main>
   );
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -4,10 +4,10 @@ import { MobileNavbar } from "./MobileNavbar"
 export const Navbar = () => {
     return (
         <>
-            <div className="inset-x-0 top-0  md:hidden">
+            <div className="inset-x-0 top-0  md:hidden" data-testid='mobile-navbar'>
                 <MobileNavbar />
             </div>
-            <div className="inset-x-0 top-0  md:block hidden">
+            <div className="inset-x-0 top-0  md:block hidden" data-testid='desktop-navbar'>
                 <DesktopNavbar />
             </div>
         </>
