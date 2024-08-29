9a731be5a0dbfb9327c8fa96f29915a23fd2328d
PedroBaneres <129055113+PedroBaneres@users.noreply.github.com>
2024-08-29 23:35:22 +0200

    add testid

--- src/components/navbar/MobileNavbar.tsx
+++ src/components/navbar/MobileNavbar.tsx
@@ -11,7 +11,7 @@ export const MobileNavbar = () => {
     const isPastNavbar = useHasPastHeight(80);
 
     return (
-        <nav className={`w-full px-6 py-2 z-40 fixed top-0 transition-all duration-200 ease-out ${isPastNavbar ? 'bg-black shadow-lg' : ' bg-transparent'}`}>
+        <nav className={`w-full px-6 py-2 z-40 fixed top-0 transition-all duration-200 ease-out ${isPastNavbar ? 'bg-black shadow-lg' : ' bg-transparent'}`} data-testid='mobile-navbar'>
             <div className="flex justify-between items-center">
                 <div>
                     <Link href='/'>
