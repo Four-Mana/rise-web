990799deae47e485c07a8bc5ef391795416aa76d
PedroBaneres <129055113+PedroBaneres@users.noreply.github.com>
2024-09-18 00:36:42 +0200

    redirect navbar links and heroSection recieving props

--- src/components/heroSection/HeroSection.tsx
+++ src/components/heroSection/HeroSection.tsx
@@ -1,22 +1,35 @@
-import Image from "next/image"
-import logo from './_assets/logotest2.png'
-import background from './_assets/bandatest.jpeg'
+import Image from "next/image";
+import { StaticImageData } from "next/image";  // Import type
 
-export const HeroSection = () => {
-    return (
-        <section className="relative max-h-screen" data-testid='hero-section'>
-            <div className="flex items-end justify-center w-full h-full">
-                <Image src={background}
-                    alt="Foto"
-                    objectFit="cover" />
-                <Image className="absolute bottom-8 max-h-" style={{
-                    width: '50%',
-                    height: 'auto',
-                }} src={logo}
-                    alt="Logo"
-                    objectFit="contain" />
-            </div>
-
-        </section>
-    )
+interface HeroSectionProps {
+  logo: StaticImageData;
+  background: StaticImageData;
 }
+
+export const HeroSection: React.FC<HeroSectionProps> = ({ logo, background }) => {
+  return (
+    <section className="relative max-h-screen" data-testid='hero-section'>
+      <div className="flex items-end justify-center w-full h-full">
+        <Image
+          style={{
+            width: '140%',
+            height: 'auto',
+          }}
+          src={background}
+          alt="Background Image"
+          objectFit="cover"
+        />
+        <Image
+          className="absolute bottom-8"
+          style={{
+            width: '50%',
+            height: 'auto',
+          }}
+          src={logo}
+          alt="Logo"
+          objectFit="contain"
+        />
+      </div>
+    </section>
+  );
+};
