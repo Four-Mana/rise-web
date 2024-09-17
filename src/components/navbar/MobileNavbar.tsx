500b8aeb98c8d4bb10e397feaf33b49a1f754212
TigreDev <donowenrt@gmail.com>
2024-09-18 01:09:14 +0200

    added missed test-id

--- src/components/navbar/MobileNavbar.tsx
+++ src/components/navbar/MobileNavbar.tsx
@@ -11,7 +11,7 @@ export const MobileNavbar = () => {
     const isPastNavbar = useHasPastHeight(80);
 
     return (
-        <nav className={`w-full px-6 py-2 z-40 fixed top-0 transition-all duration-200 ease-out ${isPastNavbar ? 'bg-black shadow-lg' : ' bg-transparent'}`}>
+        <nav data-testid='mobile-navbar' className={`w-full px-6 py-2 z-40 fixed top-0 transition-all duration-200 ease-out ${isPastNavbar ? 'bg-black shadow-lg' : ' bg-transparent'}`}>
             <div className="flex justify-between items-center">
                 <div>
                     <Link href='/'>
