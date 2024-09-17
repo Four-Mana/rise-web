ed0725a80a10dfe8b72317f824ccfb10f01328e7
PedroBaneres <129055113+PedroBaneres@users.noreply.github.com>
2024-09-18 00:36:42 +0200

    redirect navbar links and heroSection recieving props

--- src/components/navbar/DesktopNavbar.tsx
+++ src/components/navbar/DesktopNavbar.tsx
@@ -14,32 +14,47 @@ export const DesktopNavbar = () => {
                         🐯
                     </Link>
                 </div>
-                <div className="flex gap-2">
-                    <Link href='/discografia'>
-                        Discografia
-                    </Link>
+                
+                <div className="flex gap-4 text-xl  ml-auto mr-2">
+                
+                <Link href='/banda' className="text-white font-semibold italic hover:text-yellow-500">
+                    La Banda
+                </Link>
+                    
+                <Link href='/discografia' className="text-white font-semibold italic hover:text-yellow-500">
+                    Discografía
+                </Link>
+                <Link href='/tour' className="text-white font-semibold italic hover:text-yellow-500">
+                    Tour
+                </Link>
+                <Link href='/tienda' className="text-white font-semibold italic hover:text-yellow-500">
+                    Tienda
+                </Link>
+                <Link href='/contacto' className="text-white font-semibold italic hover:text-yellow-500">
+                    Contacto
+                </Link>
 
                 </div>
 
                 <div className="flex gap-3">
 
-                    <Link href="https://es-es.facebook.com/RiseOrDieOfficial">
+                    <Link href="https://es-es.facebook.com/RiseOrDieOfficial" className="text-white hover:text-yellow-500">
                         <FaFacebookF size={22} />
                     </Link>
 
-                    <Link href="https://instagram.com/rise_metalband/">
+                    <Link href="https://instagram.com/rise_metalband/" className="text-white  hover:text-yellow-500" >
                         <FaInstagram size={24} />
                     </Link>
 
-                    <Link href="https://www.youtube.com/@risebandofficial?si=b2WRmfNVw6B1kK8U">
+                    <Link href="https://www.youtube.com/@risebandofficial?si=b2WRmfNVw6B1kK8U" className="text-white hover:text-yellow-500">
                         <FaYoutube size={24} />
                     </Link>
 
-                    <Link href="https://risemetalband.bandcamp.com/album/derange#">
+                    <Link href="https://risemetalband.bandcamp.com/album/derange#" className="text-white hover:text-yellow-500">
                         <FaBandcamp size={24} />
                     </Link>
 
-                    <Link href="https://music.youtube.com/channel/UCFl15L655YrI3f4VvB5HLBg?si=aD6q0c5LFQrMjjmn">
+                    <Link href="https://music.youtube.com/channel/UCFl15L655YrI3f4VvB5HLBg?si=aD6q0c5LFQrMjjmn" className="text-white hover:text-yellow-500">
                         <SiYoutubemusic size={24} />
                     </Link>
                 </div>
