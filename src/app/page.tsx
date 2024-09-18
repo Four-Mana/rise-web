d6dee03528aa8f09a5f3d316d3ea62ffb114e8d3
PedroBaneres <129055113+PedroBaneres@users.noreply.github.com>
2024-09-19 01:36:40 +0200

    initial commit early NextEvents component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import { HeroSection } from "@/components/heroSection";
+import { NextEvents } from '@/components/nextEvents';
 import logo from '@/components/_assets/logotest2.png';
 import background from '@/components/_assets/fotogrupal2.jpg';
 
@@ -6,6 +7,7 @@ export default function Home() {
   return (
     <main className="flex h-[300vh] flex-col items-center justify-between">
       <HeroSection logo={logo} background={background} />
+      <NextEvents />
     </main>
   );
 }
