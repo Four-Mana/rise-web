f2ea94c7d0b1cd22849616a84ddb8134dc505ccb
PedroBaneres <129055113+PedroBaneres@users.noreply.github.com>
2024-10-23 00:24:50 +0200

    delete bottom svg

--- src/components/home/album.tsx
+++ src/components/home/album.tsx
@@ -8,11 +8,7 @@ import Image from 'next/image'
 export const Album = () => {
   return (
     <section className='grid md:grid-cols-2 max-w-5xl max-h-screen gap-x-6 mx-8 py-0 pb-12'>
-      <div className="divider-top-album">
-        <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
-          <path d="M985.66,92.83C906.67,72,823.78,31,743.84,14.19c-82.26-17.34-168.06-16.33-250.45.39-57.84,11.73-114,31.07-172,41.86A600.21,600.21,0,0,1,0,27.35V120H1200V95.8C1132.19,118.92,1055.71,111.31,985.66,92.83Z" className="shape-fill"></path>
-        </svg>
-      </div>
+     
       <div className="mx-auto w-5/6 aspect-square pt-20">
         <Image
           src={breathingAgain}
