8b05609f31f581a9e94234ed0d843557e515713d
TigreDev <donowenrt@gmail.com>
2024-10-24 08:56:22 +0200

    moved to client component

--- src/app/shop/page.tsx
+++ src/app/shop/page.tsx
@@ -2,8 +2,7 @@ import { Logo } from "@/components";
 
 import { Highlight } from "@/components/highlight";
 import { Album } from "@/components/home";
-import { Input } from "@headlessui/react";
-import classNames from "classnames";
+import { PurchaseForm } from "@/components/shop/purchase-form";
 
 export const metadata = {
   title: "The shop - Rise",
@@ -11,22 +10,6 @@ export const metadata = {
 }
 
 export default function Shop() {
-  const handleFormSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-
-    const formData = new FormData(event.currentTarget);
-    try {
-      await fetch('/__forms.html', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/x-www-form-urlencoded',
-        },
-        body: new URLSearchParams(formData as any).toString(),
-      });
-    } catch (error) {
-      console.error('Form submission error:', error);
-    }
-  };
 
   return (
     <main className="flex flex-col items-center justify-between mt-20 bg-zinc-900 gap-6">
@@ -40,34 +23,8 @@ export default function Shop() {
         <p className="text-center">
           Rellena tus datos y nos pondremos en contacto contigo lo antes posible
         </p>
-        <form name="purchase-form" method="POST" data-netlify="true" onSubmit={handleFormSubmit} >
-          <input type="hidden" name="purchase-form" value="purchase" />
-          <div className="flex flex-col pb-12 gap-4 w-full max-w-lg mx-auto pt-6">
-            <label className='font-semibold tracking-tighter text-white/80' htmlFor="name">Your Name: </label>
-            <Input
-              type="text" name="name"
-              className={classNames(
-                'mt-3 block w-full rounded-lg bg-zinc-600 py-1.5 px-3 text-sm/6 text-white',
-                'focus:outline-none data-[focus]:outline-2 data-[focus]:-outline-offset-2 data-[focus]:outline-white/25'
-              )}
-            />
-            <label htmlFor="email">Your Email: </label>
-            <Input type="email" name="email"
-              className={classNames(
-                'mt-3 block w-full rounded-lg bg-zinc-600 py-1.5 px-3 text-sm/6 text-white',
-                'focus:outline-none data-[focus]:outline-2 data-[focus]:-outline-offset-2 data-[focus]:outline-white/25'
-              )} />
-            <label htmlFor="message">Message: </label>
-            <textarea name="message"
-              className={classNames(
-                'mt-3 block w-full rounded-lg bg-zinc-600 py-1.5 px-3 text-sm/6 text-white',
-                'focus:outline-none data-[focus]:outline-2 data-[focus]:-outline-offset-2 data-[focus]:outline-white/25'
-              )}></textarea>
-            <button className="bg-primary-400 rounded-sm w-fit px-10 font-black text-white py-2 self-end" type="submit">Send</button>
-          </div>
-        </form>
+        <PurchaseForm />
       </div>
-
     </main >
   );
 }
---
+++ src/components/shop/purchase-form/index.tsx
@@ -0,0 +1,52 @@
+'use client'
+import { Input } from '@headlessui/react';
+import classNames from 'classnames';
+import React from 'react'
+
+export const PurchaseForm = () => {
+  const handleFormSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    const formData = new FormData(event.currentTarget);
+    try {
+      await fetch('/__forms.html', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        body: new URLSearchParams(formData as any).toString(),
+      });
+    } catch (error) {
+      console.error('Form submission error:', error);
+    }
+  };
+
+  return (
+    <form name="purchase-form" method="POST" data-netlify="true" onSubmit={handleFormSubmit} >
+      <input type="hidden" name="purchase-form" value="purchase" />
+      <div className="flex flex-col pb-12 gap-4 w-full max-w-lg mx-auto pt-6">
+        <label className='font-semibold tracking-tighter text-white/80' htmlFor="name">Your Name: </label>
+        <Input
+          type="text" name="name"
+          className={classNames(
+            'mt-3 block w-full rounded-lg bg-zinc-600 py-1.5 px-3 text-sm/6 text-white',
+            'focus:outline-none data-[focus]:outline-2 data-[focus]:-outline-offset-2 data-[focus]:outline-white/25'
+          )}
+        />
+        <label htmlFor="email">Your Email: </label>
+        <Input type="email" name="email"
+          className={classNames(
+            'mt-3 block w-full rounded-lg bg-zinc-600 py-1.5 px-3 text-sm/6 text-white',
+            'focus:outline-none data-[focus]:outline-2 data-[focus]:-outline-offset-2 data-[focus]:outline-white/25'
+          )} />
+        <label htmlFor="message">Message: </label>
+        <textarea name="message"
+          className={classNames(
+            'mt-3 block w-full rounded-lg bg-zinc-600 py-1.5 px-3 text-sm/6 text-white',
+            'focus:outline-none data-[focus]:outline-2 data-[focus]:-outline-offset-2 data-[focus]:outline-white/25'
+          )}></textarea>
+        <button className="bg-primary-400 rounded-sm w-fit px-10 font-black text-white py-2 self-end" type="submit">Send</button>
+      </div>
+    </form>
+  )
+}
