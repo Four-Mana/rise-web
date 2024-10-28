7bbc1466cbea0c253a79271c860ed02e1e8cd442
TigreDev <donowenrt@gmail.com>
2024-10-28 21:59:02 +0100

    enable website

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        Work in progress
+        <Navbar />
+        {children}
+        <Footer />
       </body>
     </html>
   );
