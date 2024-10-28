5082c17f44249341d2c247624dd4e5406ecfecdf
TigreDev <donowenrt@gmail.com>
2024-10-28 22:01:22 +0100

    avoid deploy

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
-import { Footer, Navbar } from '@/components';
+// import { Footer, Navbar } from '@/components';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -11,7 +11,7 @@ export const metadata: Metadata = {
 };
 
 export default function RootLayout({
-  children,
+  // children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
