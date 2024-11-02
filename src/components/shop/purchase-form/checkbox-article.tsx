9a9b7ae32676dc65a0d2ba2a413ffae39169237a
TigreDev <donowenrt@gmail.com>
2024-11-02 22:17:23 +0100

    refactor form

---
+++ src/components/shop/purchase-form/checkbox-article.tsx
@@ -0,0 +1,48 @@
+import { Checkbox, Select } from '@headlessui/react';
+import classNames from 'classnames';
+import React, { ChangeEvent, useState } from 'react'
+import { CgCheck, CgChevronDown } from 'react-icons/cg';
+
+export const ArticleCheckbox = ({ name, options = ["on"], price, onChange, singleOption }: { name: string, options?: string[], price: string, singleOption?: boolean, onChange: (value: string | boolean) => void }) => {
+  const [selectedOption, setSelectedOption] = useState(options[0]);
+
+  const handleOptionChange = (event: ChangeEvent<HTMLSelectElement>) => {
+    const value = event.target.value;
+    setSelectedOption(value);
+    onChange(value);
+  };
+
+  return (
+    <div className="w-full p-3 bg-white/20 ring-1 hover:ring-white/30 ring-transparent group rounded-md flex flex-col gap-4">
+      <div className="flex gap-4">
+        <Checkbox
+          onClick={() => onChange(selectedOption)}
+          name={name}
+          className="block size-6 rounded-md bg-white/10 p-1 ring-1 ring-white/15 ring-inset data-[checked]:bg-white/60 group group-hover:ring-white/30"
+        >
+          <CgCheck className="hidden size-4 text-black font-black group-data-[checked]:block" />
+        </Checkbox>
+        <label>{name}</label>
+      </div>
+      <div className="w-full flex justify-between items-center">
+        <div>{price}</div>
+        <div className={classNames("relative w-2/5 self-end", { "hidden": singleOption })}>
+          <Select value={selectedOption} onChange={handleOptionChange} className={selectClassNames} disabled={!selectedOption}>
+            {options.map((option) => (
+              <option key={option} value={option}>
+                {option}
+              </option>
+            ))}
+          </Select>
+          <CgChevronDown className="absolute top-2.5 right-2.5 size-4 fill-white/60 pointer-events-none" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+
+const selectClassNames = classNames(
+  'block w-full appearance-none rounded-lg bg-white/5 py-1.5 px-3 text-sm text-white',
+  'focus:outline-none data-[focus]:outline-2 data-[focus]:-outline-offset-2 data-[focus]:outline-white/25',
+);
--- src/components/shop/purchase-form/index.tsx
+++ src/components/shop/purchase-form/index.tsx
@@ -6,6 +6,8 @@ import { CgCheck, CgChevronDown } from 'react-icons/cg';
 import classNames from 'classnames';
 import { articlesList } from '../articlesList';
 import { Highlight } from '@/components/highlight';
+import { SuccessPurchase } from './success-purchase';
+import { ArticleCheckbox } from './checkbox-article';
 
 export const PurchaseForm = () => {
   const [isSubmitted, setIsSubmitted] = useState(false);
@@ -50,7 +52,7 @@ export const PurchaseForm = () => {
       </p>
       <input type="hidden" name="form-name" value="purchase-form" />
 
-      <div className="flex flex-col pb-12 w-full max-w-lg mx-auto pt-6 px-6 gap-4">
+      <div className="flex flex-col pb-12 w-full max-w-4xl mx-auto pt-6 px-6 gap-4">
         <label className="font-semibold tracking-tighter text-white/80" htmlFor="name">
           Nombre:
         </label>
@@ -64,7 +66,7 @@ export const PurchaseForm = () => {
         <label className="font-semibold tracking-tighter text-white/80 mb-2 block">Elige tu merch</label>
         <div className="grid md:grid-cols-2 gap-2">
           {articlesList.map((article) => (
-            <CheckboxItem
+            <ArticleCheckbox
               key={article.name}
               name={article.name}
               {...(article.variants && { options: article.variants.split(" ") })}
@@ -91,53 +93,3 @@ const inputClassNames = classNames(
   'block w-full rounded-lg bg-zinc-600 py-1.5 px-3 text-sm text-white',
   'focus:outline-none data-[focus]:outline-2 data-[focus]:-outline-offset-2 data-[focus]:outline-white/25'
 );
-
-export const CheckboxItem = ({ name, options = ["on"], price, onChange, singleOption }: { name: string, options?: string[], price: string, singleOption?: boolean, onChange: (value: string | boolean) => void }) => {
-  const [selectedOption, setSelectedOption] = useState(options[0]);
-
-  const handleOptionChange = (event: ChangeEvent<HTMLSelectElement>) => {
-    const value = event.target.value;
-    setSelectedOption(value);
-    onChange(value);
-  };
-
-  const selectClassNames = classNames(
-    'block w-full appearance-none rounded-lg bg-white/5 py-1.5 px-3 text-sm text-white',
-    'focus:outline-none data-[focus]:outline-2 data-[focus]:-outline-offset-2 data-[focus]:outline-white/25',
-  );
-  return (
-    <div className="w-full p-3 bg-white/20 ring-1 hover:ring-white/30 ring-transparent group rounded-md flex flex-col gap-4">
-      <div className="flex gap-4">
-        <Checkbox onClick={() => onChange(selectedOption || 'on')} name={name} className="block size-6 rounded-md bg-white/10 p-1 ring-1 ring-white/15 ring-inset data-[checked]:bg-white/60 group group-hover:ring-white/30">
-          <CgCheck className="hidden size-4 text-black font-black group-data-[checked]:block" />
-        </Checkbox>
-        <label>{name}</label>
-      </div>
-      <div className="w-full flex justify-between items-center">
-        <div>{price}</div>
-        <div className={classNames("relative w-2/5 self-end", { "hidden": singleOption })}>
-          <Select value={selectedOption} onChange={handleOptionChange} className={selectClassNames} disabled={!selectedOption}>
-            {options.map((option) => (
-              <option key={option} value={option}>
-                {option}
-              </option>
-            ))}
-          </Select>
-          <CgChevronDown className="absolute top-2.5 right-2.5 size-4 fill-white/60 pointer-events-none" />
-        </div>
-      </div>
-    </div>
-  );
-};
-
-
-export const SuccessPurchase = () => {
-  return (
-    <div className='flex flex-col items-center py-20'>
-      <BiCheckCircle size={64} className='text-green-400' />
-      <h3 className='tracking-tight font-light text-4xl text-white text-center pb-6 w-2/3'>
-        Tomamos nota.<br /> Te contactaremos <Highlight>pronto</Highlight> para tramitar la compra.
-      </h3>
-    </div>
-  )
-}
