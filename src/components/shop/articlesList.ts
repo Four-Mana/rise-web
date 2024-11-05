c666102e6e3f7bee51bcdcd3c5e7084787fc70d6
TigreDev <donowenrt@gmail.com>
2024-11-05 19:05:46 +0100

    added previous album

--- src/components/shop/articlesList.ts
+++ src/components/shop/articlesList.ts
@@ -2,6 +2,8 @@ import FrankFrontImg from './assets/tshirt-frank-front.png'
 import FrankBackImg from './assets/tshirt-frank-back.png'
 import AlbumFrontImg from './assets/tshirt-album-front.png'
 import AlbumImg from './assets/breathingAgain.png'
+import DerangeFrontImg from './assets/derange_front.jpg'
+import DerangeBackImg from './assets/derange_back.jpg'
 import { StaticImageData } from 'next/image';
 
 
@@ -17,27 +19,35 @@ export type articleListType = {
 
 export const articlesList: articleListType[] = [
   {
+    front: AlbumImg,
+    name: "Breathing Again Album",
+    formName: "album",
+    description: "",
+    price: "10,00€"
+  },
+  {
     front: FrankFrontImg,
     back: FrankBackImg,
     description: "",
     formName: "frank-tshirt",
     name: "Frank T-Shirt",
-    price: "50.00$",
-    variants: "XS S M L XL",
+    price: "15.00€",
+    variants: "5-6Y S M L XL",
   },
   {
     name: "Breathing Again T-Shirt",
     formName: "album-tshirt",
     front: AlbumFrontImg,
     description: "",
-    price: "50.00$",
-    variants: "XS S M L XL",
+    price: "15.00€",
+    variants: "5-6Y S M L XL",
   },
   {
-    front: AlbumImg,
-    name: "Breathing Again Album",
-    formName: "album",
+    front: DerangeFrontImg,
+    back: DerangeBackImg,
+    name: "Derange Album",
+    formName: "derange-album",
     description: "",
-    price: "50.00$"
-  }
+    price: "5.00€"
+  },
 ]
