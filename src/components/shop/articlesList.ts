ba3a3c4fed0f9881a43125cde4d4e6920c54451c
TigreDev <donowenrt@gmail.com>
2024-11-05 20:16:36 +0100

    added new packs

--- src/components/shop/articlesList.ts
+++ src/components/shop/articlesList.ts
@@ -4,6 +4,8 @@ import AlbumFrontImg from './assets/tshirt-album-front.png'
 import AlbumImg from './assets/breathingAgain.png'
 import DerangeFrontImg from './assets/derange_front.png'
 import DerangeBackImg from './assets/derange_back.png'
+import PackFrank from './assets/pack-album-frank.png'
+import PackAlbum from './assets/pack-album-tshirt.png'
 import { StaticImageData } from 'next/image';
 
 
@@ -49,5 +51,23 @@ export const articlesList: articleListType[] = [
     back: DerangeBackImg,
     description: "",
     price: "5.00€"
+  },
+  {
+    name: "Pack Frank + Album",
+    formName: "pack-Frank-tshirt",
+    front: PackFrank,
+    back: FrankBackImg,
+    description: "",
+    price: "22.00€",
+    variants: "5-6Y S M L XL",
+  },
+  {
+    name: "Pack Breathing Again",
+    formName: "pack-Frank-tshirt",
+    front: PackAlbum,
+    back: AlbumFrontImg,
+    description: "",
+    price: "22.00€",
+    variants: "5-6Y S M L XL",
   },
 ]
